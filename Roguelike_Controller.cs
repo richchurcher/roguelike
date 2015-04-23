8c4ef4688948f82987682243b573929c9822489c
user <user>
2015-04-24 11:39:19 +1200

    barebones of controller

---
+++ Roguelike_Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike
+{
+    class Roguelike_Controller
+    {
+        public DummyModel Model  { get; set; }
+        public DummyView View { get; set; }
+
+        public Roguelike_Controller()
+        {
+            Model = new DummyModel();
+            View = new DummyView();
+        }
+
+        public void MoveCharacter()
+        {
+            ConsoleKeyInfo direction = View.MoveInput();
+            List<List<String>> updatedBoard = DummyModel.DoSomethingWithMove(direction);
+            View.UpdateBoard(updatedBoard);
+        }
+
+
+
+
+
+
+    }
+}
