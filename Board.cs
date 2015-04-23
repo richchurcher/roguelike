d6f20d9a95c70d6bc2f9a0b29787b47ddbd8fb6c
Daniel <daniel.lewis@devacademy.co.nz>
2015-04-24 11:13:23 +1200

    Add board class

---
+++ Board.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Roguelike
+{
+    public class Board
+    {
+        private List<List<string>> _board;
+
+        public List<List<string>> Get()
+        {
+            return _board;
+        }
+    }
+}
