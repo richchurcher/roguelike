fe5cd6e6f45e55699d956e5d2806d14e277fe4fe
user <user@users.noreply.github.com>
2015-04-24 11:41:48 +1200

    initial display view class

---
+++ BoardView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike
+{
+    public class BoardView
+    {
+        public BoardView(List<List<string>> board )
+        {
+            for (int i = 0; i < board.Count; i++)
+            {
+                for (int j = 0; j < board.Count; j++)
+                {
+                    Console.WriteLine(board[i][j]);
+                }
+            }               
+        }
+
+        public void ClearBoard()
+        {
+            
+        }
+    }
+}
