42a1d3d8e53194ba6af27d78f48af2780646fe49
Daniel <daniel.lewis@devacademy.co.nz>
2015-04-24 12:47:02 +1200

    Save

--- Trap.cs
+++ Trap.cs
@@ -2,6 +2,19 @@
 {
     public class Trap
     {
+        public int Row { get; private set; }
+        public int Col { get; private set; }
+
+        public Trap(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+        }
+
+        public string ToString()
+        {
+            return ">";
+        }
          
     }
 }
