d9b6bd6175083d78ded391a8fdae34c56252447b
user <user>
2015-04-24 14:11:00 +1200

    Game state progress

--- Roguelike_Controller.cs
+++ Roguelike_Controller.cs
@@ -9,26 +9,38 @@ namespace Roguelike
 {
     class Roguelike_Controller
     {
-        public Board _board  { get; set; }
-        public DummyView _view { get; set; }
+        public Board _MainBoard  { get; set; }
+        public BoardView _view { get; set; }
         //public Player player { get; set; }
 
         public Roguelike_Controller()
         {
-            _board = new Board();
+            _MainBoard = new Board();
             //player = new Player(0, 0);
-            _view = new DummyView();
+            _view = new BoardView(_MainBoard.Get());
             
         }
-
+        
         public void MoveCharacter()
         {
             ConsoleKeyInfo direction = _view.MoveInput();
-            List<List<String>> updatedBoard = _board.DoSomethingWithMove(direction);
+            List<List<String>> updatedBoard = _MainBoard.DoSomethingWithMove(direction);
             _view.ClearBoard();
             _view.BoardView(updatedBoard);
             
         }
-        
+
+        public void Game()
+        {
+            while (!_MainBoard.Finished())
+            {
+                MoveCharacter();
+            }
+            Console.WriteLine("You won the game!");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+
+
     }
 }
