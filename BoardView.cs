557d1f162dcac1a35064259d5abe79e9bcf8e8cf
user <user@users.noreply.github.com>
2015-04-24 15:48:16 +1200

    Created Border

--- BoardView.cs
+++ BoardView.cs
@@ -10,14 +10,31 @@ namespace Roguelike
     {
         public void CreateBoard(List<List<string>> board)
         {
+            Console.WriteLine("@@@@@@@@@@@");
             for (int i = 0; i < board.Count; i++)
             {
+                if (i == 0)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("@");
+                }
                 for (int j = 0; j < board[i].Count; j++)
                 {
                     Console.Write(board[i][j]);
                 }
-                Console.Write("\n");
+                if (i == 8)
+                {
+                    Console.Write(" \n");
+                }
+                else
+                {
+                    Console.Write("@\n");
+                }
             }
+            Console.WriteLine("@@@@@@@@@@@");
         }
 
         public void ClearBoard()
